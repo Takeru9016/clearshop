5bb03001a908235c548ab7ec5795db98d14b9afa
Takeru9016 <youthfox046@gmail.com>
2025-12-14 15:27:14 +0530

    feat: Integrate Sanity CMS, set up Shadcn UI components, and add Clerk authentication with updated project structure.

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="">
+      {/* Featured Section */}
+
+      {/* Page Banner */}
+
+      {/* Category Tiles */}
+
+      {/* Product Section */}
+    </div>
+  );
+}
